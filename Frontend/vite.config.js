1f36b30d171e1b9f90b161717deb7d586c6d6973
phuwanart phuangbangpho <phuwanart.phuang@gmail.com>
2025-03-27 14:43:41 +0700

    vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
-
-// Convert import.meta.url to a directory path
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"), 
     },
     optimizeDeps: {
       exclude: ["i18next", "react-i18next"],
